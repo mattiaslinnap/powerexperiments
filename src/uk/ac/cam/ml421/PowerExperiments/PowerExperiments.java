9c6325b0365ec2f5141f5225c4846f479439e1fa
Mattias Linnap <mattias@linnap.com>
2011-07-30 18:55:05 +0100

    Initial project

---
+++ src/uk/ac/cam/ml421/PowerExperiments/PowerExperiments.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.ml421.PowerExperiments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PowerExperiments extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
